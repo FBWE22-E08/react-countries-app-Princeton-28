cb0a4f0ef6258c8323389daa09aa678cec1dc6e6
Prince Amanie <princeton_28jh@outlook.com>
2023-02-20 16:28:24 +0100

    exercise half completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 import AllCountries from "./components/views/AllCountries";
 import ByRegion from "./components/views/ByRegion";
 import ByName from "./components/views/ByName";
 import Header from "./components/Header";
-import NotFound from "./components/NotFound";
+import NotFound from "./components/views/NotFound";
 
 function App() {
-
   return (
     <div className="App">
-{/* show the Header component, then add the routes to the pages components*/}
+      <Header />
 
-   App component
+      <Routes>
+        <Route path="/" element={<AllCountries />} />
+        <Route path="/by-region" element={<ByRegion />} />
+        <Route path="/by-name" element={<ByName />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
--- src/components/views/ByRegion.js
+++ src/components/views/ByRegion.js
@@ -5,10 +5,13 @@ export default function ByRegion() {
   //save the user input in the pre-made local state (region), then fetch all countries of that region on page load and make sure it gets executed every time the state (region) changes (what hook should you use for that? and what should you add to it?) then save the received array of countries in a local state (call it countries)
 
   const [region, setRegion] = useState("Africa");
-  
 
-  const handleChange = () => {};
-
+  useEffect(() => {
+    fetch("https://restcountries.com/v2/region/{region}")
+      .then((response) => response.json())
+      .then((data) => setRegion(data));
+  }, []);
+  // const handleChange = () => {};
 
   return (
     <div>
